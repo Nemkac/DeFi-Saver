8f818c29b6c97060c4642c950f4a547f0ba72c86
Nemanja Todorović <77896776+Nemkac@users.noreply.github.com>
2026-03-22 21:45:39 +0100

    Moved count label under the filters

--- app/src/widgets/cdp/filters/cdp-filters-section.tsx
+++ app/src/widgets/cdp/filters/cdp-filters-section.tsx
@@ -18,14 +18,16 @@ type DataFiltersProps = {
 
 const DataFilters = ({ ...props }: DataFiltersProps) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full items-center gap-4">
-            <SelectDropdown
-                label="Collateral Type"
-                value={props.collateral as 'ETH-A' | 'WBTC-A' | 'USDC-A' | null}
-                onChange={props.onCollateralChange}
-                options={COLLATERAL_OPTIONS}
-            />
-            <InputField title='CDP ID: ' placeholder='Enter CDP ID' value={props.cdpId} onChange={props.onCdpIdChange} />
+        <div className='flex flex-col gap-4 w-full'>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full items-center gap-4">
+                <SelectDropdown
+                    label="Collateral Type"
+                    value={props.collateral as 'ETH-A' | 'WBTC-A' | 'USDC-A' | null}
+                    onChange={props.onCollateralChange}
+                    options={COLLATERAL_OPTIONS}
+                />
+                <InputField title='CDP ID: ' placeholder='Enter CDP ID' value={props.cdpId} onChange={props.onCdpIdChange} />
+            </div>
             <p className='text-p-md text-on-surface-secondary'>{props.countLabel}</p>
         </div>
     )
